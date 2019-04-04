dbad176bc9d8ed99628b20a331765b28558a39f6
Zweer <n.olivieriachille@gmail.com>
2019-04-04 18:37:45 +0200

    Fixed describes

--- src/lambda/errors.spec.ts
+++ src/lambda/errors.spec.ts
@@ -1,48 +1,46 @@
 import { ErrorWithStatus, ErrorWithStatus404 } from './errors';
 
-describe('Lambda', () => {
-  describe('Errors', () => {
-    describe('Error with Status', () => {
-      it('should create an error', () => {
-        const errorWithStatus = new ErrorWithStatus(200);
+describe('Errors', () => {
+  describe('Error with Status', () => {
+    it('should create an error', () => {
+      const errorWithStatus = new ErrorWithStatus(200);
 
-        expect(errorWithStatus).toBeInstanceOf(Error);
-      });
-
-      it('should inherit the "message" property', () => {
-        const message = 'message';
-        const errorWithStatus = new ErrorWithStatus(200, message);
-
-        expect(errorWithStatus).toHaveProperty('message', message);
-      });
-
-      it('should have the "statusCode" property', () => {
-        const statusCode = 200;
-        const errorWithStatus = new ErrorWithStatus(statusCode);
-
-        expect(errorWithStatus).toHaveProperty('statusCode', statusCode);
-      });
+      expect(errorWithStatus).toBeInstanceOf(Error);
     });
 
-    describe('Status Code 404', () => {
-      it('should be an ErrorWithStatus', () => {
-        const error404 = new ErrorWithStatus404();
+    it('should inherit the "message" property', () => {
+      const message = 'message';
+      const errorWithStatus = new ErrorWithStatus(200, message);
 
-        expect(error404).toBeInstanceOf(ErrorWithStatus);
-      });
+      expect(errorWithStatus).toHaveProperty('message', message);
+    });
 
-      it('should have the 404 statusCode', () => {
-        const error404 = new ErrorWithStatus404();
+    it('should have the "statusCode" property', () => {
+      const statusCode = 200;
+      const errorWithStatus = new ErrorWithStatus(statusCode);
 
-        expect(error404).toHaveProperty('statusCode', 404);
-      });
+      expect(errorWithStatus).toHaveProperty('statusCode', statusCode);
+    });
+  });
 
-      it('should have the "message" property', () => {
-        const message = 'message';
-        const error404 = new ErrorWithStatus404(message);
+  describe('Status Code 404', () => {
+    it('should be an ErrorWithStatus', () => {
+      const error404 = new ErrorWithStatus404();
 
-        expect(error404).toHaveProperty('message', message);
-      });
+      expect(error404).toBeInstanceOf(ErrorWithStatus);
+    });
+
+    it('should have the 404 statusCode', () => {
+      const error404 = new ErrorWithStatus404();
+
+      expect(error404).toHaveProperty('statusCode', 404);
+    });
+
+    it('should have the "message" property', () => {
+      const message = 'message';
+      const error404 = new ErrorWithStatus404(message);
+
+      expect(error404).toHaveProperty('message', message);
     });
   });
 });
